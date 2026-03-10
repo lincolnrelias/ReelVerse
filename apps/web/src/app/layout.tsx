649a869f8466bf232ce55d86a3c3149aaad7db6b
Lincoln Elias <lincolnreistkd@gmail.com>
2026-03-10 20:35:49 -0300

    fix(web): change AdSense script strategy to afterInteractive

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
             async
             src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_ID}`}
             crossOrigin="anonymous"
-            strategy="lazyOnload"
+            strategy="afterInteractive"
           />
         )}
       </head>
